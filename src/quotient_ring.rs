517d64876bc6190935c3ae1a4813068a6944e507
Kasper <kasperdi1412@gmail.com>
2022-02-12 22:31:36 +0100

    Removed redundant print statements. Added trimming to rest of pol methods.

--- src/quotient_ring.rs
+++ src/quotient_ring.rs
@@ -13,23 +13,17 @@ impl Rq {
     }
     // Returns the remainder found by doing polynomial long division https://rosettacode.org/wiki/Polynomial_long_division
     pub fn reduce(&self, pol: &Polynomial) -> Polynomial {
-        let q = 0;
+        //let q = 0;
         let mut r = pol.clone();
 
         while r != vec![0] && r.len() >= self.modulo.len() {
-            println!("r: {:?}", r);
-            println!("{:?}", self.modulo[self.modulo.len() - 1]);
             let t = r[r.len() - 1] / self.modulo[self.modulo.len() - 1];
-            println!("n: {:?}", self.modulo);
-            println!("t: {}", t);
-            let q = q + t;
+            //let q = q + t;
+
             let to_shift = &pol_neg(&pol_times(&self.modulo, t));
             let extra_zeros = vec![0; r.len() - self.modulo.len()];
-            println!("extra: {:?}", extra_zeros);
             let shifted_vec = [extra_zeros.as_slice(), to_shift.as_slice()].concat();
-            println!("shifted: {:?}", shifted_vec);
             r = pol_add(&r, &shifted_vec);
-            println!("r: {:?}", r);
         };
         // Reduce coefficients mod q
         mod_coefficients(&r, self.q)
@@ -57,7 +51,7 @@ impl Rq {
 }
 
 pub fn mod_coefficients(pol: &Polynomial, modulus: i128) -> Polynomial {
-    pol.iter().map(|x| x.rem_euclid(modulus)).collect()
+    pol_trim_res(&pol.iter().map(|x| x.rem_euclid(modulus)).collect())
 }
 
 pub fn pol_add(a: &Polynomial, b: &Polynomial) -> Polynomial {
@@ -87,11 +81,11 @@ pub fn pol_mul(a: &Polynomial, b: &Polynomial) -> Polynomial {
 }
 
 pub fn pol_times(pol: &Polynomial, t: i128) -> Polynomial {
-    pol.iter().map(|x| x * t ).collect()
+    pol_trim_res(&pol.iter().map(|x| x * t ).collect())
 }
 
 pub fn pol_neg(pol: &Polynomial) -> Polynomial {
-    pol.iter().map(|x| -x ).collect()
+    pol_trim_res(&pol.iter().map(|x| -x ).collect())
 }
 
 pub fn pol_trim_res(pol: &Polynomial) -> Polynomial {
@@ -99,9 +93,6 @@ pub fn pol_trim_res(pol: &Polynomial) -> Polynomial {
     let mut res = pol.clone();
     while let Some(true) = res.last().map(|x| *x == 0) {
         res.pop();
-        if res == vec![0] {
-            break
-        }
     };
     res
 }
