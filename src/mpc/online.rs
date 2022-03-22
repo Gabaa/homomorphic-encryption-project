b1bfc595d6f750df20ca9bb402cecd7e10b6a79c
kasperdi di <kasperdi1412@gmail.com>
2022-03-22 13:52:35 +0100

    Set up files for online protocol

--- src/mpc/online.rs
+++ src/mpc/online.rs
@@ -1,1 +1,21 @@
 //! Online phase (Multiparty Computation from Somewhat Homomorphic Encryption, sec. 2)
+
+pub struct ProtocolOnline {}
+
+impl ProtocolOnline {
+    pub fn input() {
+        
+    }
+
+    pub fn Add() {
+        
+    }
+
+    pub fn multiply() {
+        
+    }
+
+    pub fn output() {
+        
+    }
+}
