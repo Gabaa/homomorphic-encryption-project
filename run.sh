826441993b58b4dd929b37e56fa80078b489c33d
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2022-04-19 13:11:56 +0200

    Fix run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cargo build --release
-cargo run --bin dealer --release > dealer.log &
-cargo run --bin player --release > player1.log &
-cargo run --bin player --release > player2.log
+
+cargo run --bin dealer --release > dealer.log 2>/dev/null &
+sleep 5
+
+cargo run --bin player --release > player1.log 2>/dev/null &
+cargo run --bin player --release > player2.log 2>/dev/null &
 echo "Running!"
