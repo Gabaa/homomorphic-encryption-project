8b9c374fbced196bf6c5b1f4f1ba22bcc5a1c40b
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2022-04-19 13:39:35 +0200

    Implemented hashing for ZKPoPK

--- src/mpc/zk.rs
+++ src/mpc/zk.rs
@@ -1,27 +1,30 @@
+use num::{integer::sqrt, BigInt};
+use sha2::digest::{ExtendableOutput, Update, XofReader};
 use sha3::Shake256;
-use crate::mpc::PlayerState;
-use crate::mpc::PublicKey;
-use crate::mpc::encrypt_det;
-use crate::mpc::encode;
-use crate::mpc::encrypt;
-use crate::mpc::Ciphertext;
-use crate::protocol::Facilitator;
-use crate::mpc::{diag, Parameters};
-use crate::prob::sample_from_uniform;
-use crate::prob::sample_single;
-use num::integer::sqrt;
-use num::BigInt;
-use num::One;
+
+use crate::{
+    mpc::{diag, encode, encrypt_det, Ciphertext, Parameters, PlayerState},
+    poly::Polynomial,
+    prob::{sample_from_uniform, sample_single},
+    protocol::Facilitator,
+};
+
+const SEC: usize = 40;
 
 // Supposed to be split into part for prover and part for verifier
-pub fn zkpopk<F: Facilitator>(params: &Parameters, e_fi: Ciphertext, sec: usize, diagonal: bool, state: &PlayerState<F>) -> bool {
-    let v = 2 * sec - 1;
+pub fn zkpopk<F: Facilitator>(
+    params: &Parameters,
+    e_fi: Ciphertext,
+    diagonal: bool,
+    state: &PlayerState<F>,
+) -> bool {
+    let v = 2 * SEC - 1;
     let tau: BigInt = &params.t / BigInt::from(2_i32);
     let rho = BigInt::from(2_i32) * BigInt::from(params.r as i64) * sqrt(params.n);
     let d = params.n * 3;
 
-    let y_i_bound = BigInt::from(128_i32) * params.n * tau * BigInt::from(sec).pow(2);
-    let s_i_bound = BigInt::from(128_i32) * d * rho * BigInt::from(sec).pow(2);
+    let y_i_bound = BigInt::from(128_i32) * params.n * tau * BigInt::from(SEC).pow(2);
+    let s_i_bound = BigInt::from(128_i32) * d * rho * BigInt::from(SEC).pow(2);
 
     let mut y = Vec::with_capacity(v);
     let mut s = Vec::with_capacity(v);
@@ -33,7 +36,8 @@ pub fn zkpopk<F: Facilitator>(params: &Parameters, e_fi: Ciphertext, sec: usize,
             m_i = diag(params, m_i)
         }
         let encoded_m_i = encode(m_i);
-        let u_i = sample_from_uniform(&((y_i_bound.clone() / params.t.clone()) - 1), params.n) * params.t.clone();
+        let u_i = sample_from_uniform(&((y_i_bound.clone() / params.t.clone()) - 1), params.n)
+            * params.t.clone();
         y.push(encoded_m_i + u_i);
 
         s.push((
@@ -48,9 +52,22 @@ pub fn zkpopk<F: Facilitator>(params: &Parameters, e_fi: Ciphertext, sec: usize,
     // The prover sends a to the verifier.
 
     let mut hasher = Shake256::default();
-    /* hasher.update(a[0]);
-    hasher.update(e_fi[0]); */
-
+    for ciphertext in a {
+        for p in ciphertext {
+            hasher.update(&polynomial_to_bytes(&p));
+        }
+    }
+    for p in e_fi {
+        hasher.update(&polynomial_to_bytes(&p));
+    }
+    let mut reader = hasher.finalize_xof();
+    let mut output = [0_u8; SEC / 8];
+    reader.read(&mut output);
 
     todo!()
+}
+
+fn polynomial_to_bytes(p: &Polynomial) -> Vec<u8> {
+    let json = serde_json::to_string(p).unwrap();
+    json.as_bytes().to_owned()
 }
