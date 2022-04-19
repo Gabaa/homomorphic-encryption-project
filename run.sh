a14ab003f8893c2b492e7bcc2534dcac0537c2e8
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2022-04-19 13:13:53 +0200

    Move logs to logs folder

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mkdir logs
+
 cargo build --release
 
-cargo run --bin dealer --release > dealer.log 2>/dev/null &
+cargo run --bin dealer --release > logs/dealer.log 2>/dev/null &
 sleep 5
 
-cargo run --bin player --release > player1.log 2>/dev/null &
-cargo run --bin player --release > player2.log 2>/dev/null &
+cargo run --bin player --release > logs/player1.log 2>/dev/null &
+cargo run --bin player --release > logs/player2.log 2>/dev/null &
 echo "Running!"
