674a26287e067ad7d2c259bf6089b488c64e4e26
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2022-04-26 12:08:27 +0200

    Make M_e matrix

--- src/mpc/zk.rs
+++ src/mpc/zk.rs
@@ -14,10 +14,11 @@ const SEC: usize = 40;
 // Supposed to be split into part for prover and part for verifier
 pub fn make_zkpopk(
     params: &Parameters,
+    x: Polynomial,
     c: Ciphertext,
     diagonal: bool,
     pk: &PublicKey,
-) -> ((), (), ()) {
+) -> (Vec<Vec<Polynomial>>, (), ()) {
     let v = 2 * SEC - 1;
     let tau: BigInt = &params.t / BigInt::from(2_i32);
     let rho = BigInt::from(2_i32) * BigInt::from(params.r as i64) * sqrt(params.n);
@@ -52,26 +53,52 @@ pub fn make_zkpopk(
     // The prover sends a to the verifier.
 
     let mut hasher = Shake256::default();
-    for ciphertext in a {
+    for ciphertext in &a {
         for p in ciphertext {
-            hasher.update(&polynomial_to_bytes(&p));
+            hasher.update(&polynomial_to_bytes(p));
         }
     }
     for p in c {
         hasher.update(&polynomial_to_bytes(&p));
     }
     let mut reader = hasher.finalize_xof();
-    let mut output = [0_u8; SEC / 8];
-    reader.read(&mut output);
+    let mut e_bytes = [0_u8; SEC / 8];
+    reader.read(&mut e_bytes);
 
-    for i in 0..output.len() {
-        println!("{:b}", i);
+    let mut e_bit_string = String::new();
+    for e_byte in e_bytes {
+        e_bit_string = format!("{}{:08b}", e_bit_string, &e_byte);
     }
 
-    todo!()
+    let mut e_bits: Vec<u8> = Vec::new();
+    for c in e_bit_string.chars() {
+        match c {
+            '0' => e_bits.push(0),
+            '1' => e_bits.push(1),
+            _ => unreachable!("should only have 0's and 1's"),
+        }
+    }
+
+    let mut m_e = vec![vec![0_u8; v]; SEC];
+    for (i, row) in m_e.iter_mut().enumerate() {
+        for (k, item) in row.iter_mut().enumerate() {
+            let e_index = i as i32 - k as i32;
+
+            if (0..(SEC as i32)).contains(&e_index) {
+                let e_index = e_index as usize;
+                *item = e_bits[e_index];
+            }
+        }
+    }
+
+    let mut z = y.clone();
+
+    let t = ();
+
+    (a, z, t)
 }
 
-pub fn verify_zkpopk(a: (), z: (), t: ()) -> bool {
+pub fn verify_zkpopk(a: Vec<Vec<Polynomial>>, z: (), t: ()) -> bool {
     true
 }
 
