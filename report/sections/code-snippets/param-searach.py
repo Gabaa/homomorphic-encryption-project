81d918bb442d7ba6d069f775288c899fa522785d
sellebjergen <sellebjergen@gmail.com>
2022-05-10 01:11:24 +0200

    Wrote what I could on the MPC choosing of parameters.

---
+++ report/sections/code-snippets/param-searach.py
@@ -0,0 +1,22 @@
+from math import sqrt, floor, log2
+
+N = 512
+p = 64
+r = 3.2
+SEC = 40
+C_m = 8.6
+n = 3
+
+# The values for the formulas
+csec = 9 * N**2 * SEC**4 * 2**(SEC + 8)
+Y = p/2 + p * (4 * C_m * r**2 * N**2 + 2 * sqrt(N) * r + 4 * C_m * r**2 * N**2)
+Z = C_m * N**2 * n**2 * csec**2 * Y**2 + n * csec * Y
+
+q_size = 2 * Z * (1 + 2**SEC)
+print("The bit size of p should be above: ", floor(log2(q_size)))
+
+chosen_q = 6440092097492369874468694478456476902429935263779065830479393474203066496323859298183983608879
+gamma = 1.005
+t_prime = sqrt(N * (log2(chosen_q)/log2(gamma)))
+r_bound = max(3.2, 1.5 * gamma**(-t_prime) * chosen_q**(1 - (N / t_prime)))
+print("The value of the randomness r should be above: ", r_bound)
